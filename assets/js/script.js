3c835f7ad15d937e30d62b9478da1526bc209ac8
Lauren Goss <laurenlgoss98@gmail.com>
2021-07-14 16:53:32 -0400

    pseudocoded js

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// Add event listener to search button
+
+// Upon button click,
+    // Fetch current weather data from API
+        // Temp
+        // Wind
+        // Humidity
+        // UV index
+
+        // Render information to page
+            // Display name of city
+
+            // Display today's date
+
+            // Display icon representing weather conditions
+
+            // Color code UV index
+
+    // Fetch 5-day forecast data from API
+        // Temp
+        // Wind
+        // Humidity
+
+        // Render information to page
+            // For each card,
+                // Display future date
+
+                // Display icon representing weather conditions
+
+    // Add current search to local storage
+        // Render cities from local storage to page
+
+        // Add event listener to city buttons
+
+            // Upon button click,
+                // Repeat search button function with that city's input
