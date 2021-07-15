0601d9057d88bc4e885bc41b676d3573f37ca152
Lauren Goss <laurenlgoss98@gmail.com>
2021-07-15 11:07:36 -0400

    added function to fetch lat/lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,43 @@
 var searchButtonEl = document.querySelector("#custom-search-button");
 var cityInputEl = document.querySelector("#city-input")
 
+function init() {
+
+}
+
 // Add event listener to search button
 searchButtonEl.addEventListener("click", handleSearch);
 
 // Upon button click,
 function handleSearch() {
     var cityInput = cityInputEl.value.trim();
-    
+
     fetchLatLon(cityInput);
 }
+
+function fetchLatLon(cityName) {
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=c8aa884e6f28d929f55e9ba1856815bd")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+
+            fetchWeather(lat, lon);
+        })
+}
+
     // Fetch current weather data from API
         // Temp
         // Wind
         // Humidity
         // UV index
-
-function fetchLatLon(cityName) {
-    
+function fetchWeather(lat, lon) {
+    console.log(lat);
+    console.log(lon);
 }
+
         // Render information to page
             // Display name of city
 
@@ -46,3 +65,5 @@ function fetchLatLon(cityName) {
 
             // Upon button click,
                 // Repeat search button function with that city's input
+
+init();
